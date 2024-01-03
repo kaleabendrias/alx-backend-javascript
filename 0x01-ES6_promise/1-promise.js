7bcb97e965b671f550048156f17dc28f177c0f02
kaleabendrias <kaleabendrias1212@gmail.com>
2024-01-03 12:58:34 +0300

    Refactor getFullResponseFromAPI function

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
-const getFullResponseFromAPI = (success) => new Promise((resolve, reject) => {
-  if (success) {
-    resolve({ body: 'success', status: 200 });
-  } else {
-    reject(new Error('The fake API is not working currently'));
-  }
-});
-
-export default getFullResponseFromAPI;
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ body: 'Success', status: 200 });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
