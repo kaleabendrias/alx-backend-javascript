986794c5fe94da6e8b57cdba190304861a8d6c16
kaleabendrias <kaleabendrias1212@gmail.com>
2024-01-05 01:20:45 +0300

    Refactor Building class constructor and sqft property

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,20 +2,13 @@
 export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building
-	&& this.evacuationWarningMessage === undefined) {
+      && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-    this.sqft = sqft;
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(val) {
-    if (typeof val !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
-    this._sqft = val;
   }
 }
