a15f4f13949f2241360aba0644b590161cf480bf
kaleabendrias <kaleabendrias1212@gmail.com>
2024-01-04 15:07:45 +0300

    Add Building class and test for subclass

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (typeof newSqft === 'number') {
+      this._sqft = newSqft;
+    } else {
+      throw new TypeError('sqft must be a number');
+    }
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
