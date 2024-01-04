460461d3e858bf93a9e9a23d51fa7ee39472b411
kaleabendrias <kaleabendrias1212@gmail.com>
2024-01-05 01:10:20 +0300

    Refactor Airport class constructor and toString method

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,34 +1,10 @@
 export default class Airport {
   constructor(name, code) {
-    this.name = name;
-    this.code = code;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  set name(newName) {
-    if (typeof newName === 'string') {
-      this._name = newName;
-    } else {
-      throw new TypeError('name must be a string');
-    }
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(newCode) {
-    if (typeof newCode === 'string') {
-      this._code = newCode;
-    } else {
-      throw new TypeError('code must be a string');
-    }
+    this._name = name;
+    this._code = code;
   }
 
   toString() {
-    return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+    return `[object ${this._code}]`;
   }
 }
