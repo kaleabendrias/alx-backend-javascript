bcdf5611eec48f89d459f83192dc8fcb11dac709
kaleab <kaleabendrias1212@gmail.com>
2024-01-08 20:13:23 +0300

    repaired lint

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,6 @@
 function getStudentsByLocation(students, city) {
   const filtered = [];
+  // eslint-disable-next-line array-callback-return
   students.filter((student) => {
     if (student.location === city) {
       filtered.push(student);
