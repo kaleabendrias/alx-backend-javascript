0232e0e6bb15e18b146d1d2b06ea10fb263d4d1b
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-28 21:33:30 +0300

    Add floating point number tests to calculateNumber function

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,8 +1,21 @@
 const assert = require('assert');
-const calculateNumber = require("./0-calcul")
+const calculateNumber = require("./0-calcul");
+const { callbackify } = require('util');
 
 describe('calculate', () => {
         it('should add two positive numbers correctly', () => {
             assert.equal(calculateNumber(3, 5), 8)
+        })
+        it('test it with floating first var', () => {
+            assert.strictEqual(calculateNumber(1.0, 3), 4);
+            assert.strictEqual(calculateNumber(1.2, 3), 4);
+        })
+        it('test it with floating second var', () => {
+            assert.strictEqual(calculateNumber(1, 2.0), 3)
+            assert.strictEqual(calculateNumber(1, 2.6), 4)
+        })
+        it('test it with floating second var', ()=> {
+            assert.strictEqual(calculateNumber(1.9, 2.0), 4)
+            assert.strictEqual(calculateNumber(1.9, 2.6), 5)
         })
 })
