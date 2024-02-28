aa4e1f35e416d4906981c48b1eb4dcbb69187d03
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-28 21:26:46 +0300

    Update dependencies and fix import in unit test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,10 +1,8 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calculate');
+const calculateNumber = require("./0-calcul")
 
 describe('calculate', () => {
-    describe('addition', () => {
         it('should add two positive numbers correctly', () => {
             assert.equal(calculateNumber(3, 5), 8)
         })
-    })
 })
