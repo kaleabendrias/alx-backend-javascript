e7d772addba8bb993352e2a7895dba9f076ff5ea
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-28 21:38:24 +0300

    Add floating point tests to calculateNumber function

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -9,17 +9,16 @@ describe('calculate', () => {
         it('test it with floating first var', () => {
             assert.strictEqual(calculateNumber(1.0, 3), 4);
             assert.strictEqual(calculateNumber(1.2, 3), 4);
+            assert.strictEqual(calculateNumber(1.7, 3), 5);
         })
         it('test it with floating second var', () => {
             assert.strictEqual(calculateNumber(1, 2.0), 3)
             assert.strictEqual(calculateNumber(1, 2.6), 4)
+            assert.strictEqual(calculateNumber(1, 2.2), 3)
         })
         it('test it with floating second var', () => {
             assert.strictEqual(calculateNumber(1.9, 2.0), 4)
             assert.strictEqual(calculateNumber(1.9, 2.6), 5)
-        })
-        it('test with .0', () => {
-            assert.strictEqual(calculateNumber(1.0, 2.0), 3)
-            assert.strictEqual(calculateNumber(1.0, 2.6), 4)
+            assert.strictEqual(calculateNumber(1.2, 2.6), 4)
         })
 })
