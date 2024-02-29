be3ccc23db41aa727b16dbd4e9b190db0d3b187e
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-29 20:11:51 +0300

    Add API code and unit tests

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,19 @@
+const request = require("request");
+const expect = require("chai").expect;
+
+describe("Cart page", () => {
+  it("should return status code 200 when id is a number", (done) => {
+    request.get("http://localhost:7865/cart/12", (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal("Payment methods for cart 12");
+      done();
+    });
+  });
+
+  it("should return status code 404 when id is not a number", (done) => {
+    request.get("http://localhost:7865/cart/hello", (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
