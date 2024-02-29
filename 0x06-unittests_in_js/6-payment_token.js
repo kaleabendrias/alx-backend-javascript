e315b2108bbdd0240b4ca5b2b41460c7aa24b584
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-29 19:36:56 +0300

    Fix error handling in getPaymentTokenFromAPI function

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -4,8 +4,6 @@ function getPaymentTokenFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({ data: 'Successful response from the API' });
-    } else {
-      reject();
     }
   });
 }
