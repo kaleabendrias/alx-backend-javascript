fc7e1617521d0188e0a1787af37d3846fc42f783
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-29 19:35:07 +0300

    Fix getPaymentTokenFromAPI test case

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -8,19 +8,6 @@ describe('getPaymentTokenFromAPI function', () => {
         expect(data).to.deep.equal({ data: 'Successful response from the API' });
         done();
       })
-      .catch((error) => {
-        done(error);
-      });
+      
   });
-
-  it('should not resolve when success is false', (done) => {
-    getPaymentTokenFromAPI(false)
-      .then(() => {
-        done(new Error('Promise should not be resolved'));
-      })
-      .catch((error) => {
-        expect(error.message).to.equal('Promise should not be resolved');
-        done();
-      });
-  });
-});
+})
