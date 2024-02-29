fe34e710c58ea6bc92f353e7ce6d883cf16602bc
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-29 19:29:39 +0300

    Add payment token module and unit tests

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,26 @@
+const { getPaymentTokenFromAPI } = require('./6-payment_token');
+const { expect } = require('chai');
+
+describe('getPaymentTokenFromAPI function', () => {
+  it('should resolve with the correct data when success is true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((data) => {
+        expect(data).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((error) => {
+        done(error);
+      });
+  });
+
+  it('should not resolve when success is false', (done) => {
+    getPaymentTokenFromAPI(false)
+      .then(() => {
+        done(new Error('Promise should not be resolved'));
+      })
+      .catch((error) => {
+        expect(error.message).to.equal('Promise should not be resolved');
+        done();
+      });
+  });
+});
