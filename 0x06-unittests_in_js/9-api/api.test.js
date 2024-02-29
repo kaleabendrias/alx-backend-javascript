a3cec7341bb64d9252e86da5df8e2bf9e5e3ce39
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-29 20:15:44 +0300

    Add unit test for API endpoint

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,6 +2,13 @@ const request = require("request");
 const expect = require("chai").expect;
 
 describe("Cart page", () => {
+  it("should return status code 200", (done) => {
+  request.get("http://localhost:7865/", (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+  });
+  });
   it("should return status code 200 when id is a number", (done) => {
     request.get("http://localhost:7865/cart/12", (error, response, body) => {
       expect(response.statusCode).to.equal(200);
